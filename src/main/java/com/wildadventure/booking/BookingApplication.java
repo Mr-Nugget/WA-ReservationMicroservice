64abab1829de9d5526b6508be9cd7f1aa0be8733
Titouan Raimbault <t_raimbault@yahoo.fr>
2020-09-14 16:20:28 +0200

    #WA-46 : Configure discovery for Eureka Server

--- src/main/java/com/wildadventure/booking/BookingApplication.java
+++ src/main/java/com/wildadventure/booking/BookingApplication.java
@@ -2,12 +2,14 @@ package com.wildadventure.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableFeignClients("com.wildadventure")
 @EnableTransactionManagement
+@EnableDiscoveryClient
 public class BookingApplication {
 
 	public static void main(String[] args) {
