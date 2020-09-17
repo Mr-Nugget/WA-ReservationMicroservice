2fba45d0655c4e24e485471ed5c36554eef0452a
Titouan Raimbault <t_raimbault@yahoo.fr>
2020-09-17 15:15:56 +0200

    #WA-49 : configure CORS policy

--- src/main/java/com/wildadventure/booking/controllers/BookingController.java
+++ src/main/java/com/wildadventure/booking/controllers/BookingController.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +23,6 @@ import com.wildadventure.booking.models.UpdatePaymentRequest;
 import com.wildadventure.booking.models.UserBookingsResponse;
 import com.wildadventure.booking.services.IBookingService;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/Booking")
 public class BookingController {
--- src/main/java/com/wildadventure/booking/controllers/CartController.java
+++ src/main/java/com/wildadventure/booking/controllers/CartController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +16,6 @@ import com.wildadventure.booking.exceptions.CartNotFoundException;
 import com.wildadventure.booking.models.Cart;
 import com.wildadventure.booking.services.ICartService;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/Cart")
 public class CartController {
